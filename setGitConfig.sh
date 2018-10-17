defb7dddd6d220fe4bd13cc18b5304dad0500491
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2018-10-17 14:21:37 +0200

    use credential store

--- setGitConfig.sh
+++ setGitConfig.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 if [ -n "$CI_USER" ]; then
-	git config --global url."https://${CI_USER}:${CI_TOKEN}@git.f-i-ts.de".insteadOf "https://git.f-i-ts.de"
+	git config --global credential.helper store
+	echo "https://${CI_USER}:${CI_TOKEN}@git.f-i-ts.de" >> ~/.git-credentials
 fi
